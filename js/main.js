16cfba784ab86b2f58a0b9e8e8ff45c2633df493
kang-gwanghun <gniusk614@nate.com>
2026-03-13 17:28:38 +0900

    Refine sec4 and sec5 mobile layout

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $(function () {
   var $menuButton = $(".btn-menu");
   var $gnb = $(".gnb");
   var $gnbItems = $(".gnb-item");
-  var $hoverQuickMenu = $("#hoverQuickMenu");
+  var $headerQuickMenu = $("#headerQuickMenu");
   var $sec2Tabs = $("#sec2Tabs");
-  var $sec2Image = $("#sec2 .dul-2371");
+  var $sec2Image = $("#sec2 .sec2-treatment-image");
   var $sec2MainTitleMarquee = $("#sec2MainTitleMarquee");
   var $sec2MainTitle = $("#sec2MainTitle");
   var $sec2MainTitleClone = $("#sec2MainTitleClone");
@@ -18,25 +18,25 @@ $(function () {
   var sec7CarouselIndex = 0;
   var sec2TabData = {
     pico: {
-      image: "./assets/images/main/sec2-dul-2371.png",
+      image: "./assets/images/main/tattoo-removal-treatment.png",
       mainTitleParts: ["통증은 줄이고", "효과는 높이고"],
       serviceTitle: "피코 문신제거",
       serviceDesc: "피코레이저로 문신에 맞춰 효과적이고 세밀한 시술이 가능합니다."
     },
     semi: {
-      image: "./assets/images/main/sec2-dul-2371.png",
+      image: "./assets/images/main/tattoo-removal-treatment.png",
       mainTitleParts: ["잔흔은 줄이고", "자연스러움은 높이고"],
       serviceTitle: "반영구 문신제거",
       serviceDesc: "예민한 부위인 만큼 섬세한 조사로 부작용을 줄이며 효과를 높입니다."
     },
     scalp: {
-      image: "./assets/images/main/sec2-dul-2371.png",
+      image: "./assets/images/main/tattoo-removal-treatment.png",
       mainTitleParts: ["두피 상태에 맞춰", "안전하게 진행하는 문신제거"],
       serviceTitle: "두피 문신제거",
       serviceDesc: "잘못되었거나 번진 두피 문신도 두피와 머리카락의 손상을 최소화하여 시술합니다."
     },
     surgical: {
-      image: "./assets/images/main/sec2-dul-2371.png",
+      image: "./assets/images/main/tattoo-removal-treatment.png",
       mainTitleParts: ["부위 특성에 맞춘", "수술적 문신제거"],
       serviceTitle: "수술적 문신제거",
       serviceDesc: "면접이나 결혼 등으로 빠른 시일 내 제거를 해야 하는 분들께 추천드립니다."
@@ -69,33 +69,33 @@ $(function () {
     {
       round: "[손가락] 5회차",
       caption: "시술 후 4개월 경과",
-      beforeImageUrl: "./assets/images/main/DUL_2346 copy.png",
+      beforeImageUrl: "./assets/images/main/before-after-sample-1.png",
       beforePosition: "34% center",
-      afterImageUrl: "./assets/images/main/sec2-dul-2371.png",
+      afterImageUrl: "./assets/images/main/tattoo-removal-treatment.png",
       afterPosition: "58% center"
     },
     {
       round: "[팔] 3회차",
       caption: "시술 후 2개월 경과",
-      beforeImageUrl: "./assets/images/main/sec2-dul-2371.png",
+      beforeImageUrl: "./assets/images/main/tattoo-removal-treatment.png",
       beforePosition: "28% center",
-      afterImageUrl: "./assets/images/main/DUL_2346 copy.png",
+      afterImageUrl: "./assets/images/main/before-after-sample-1.png",
       afterPosition: "66% center"
     },
     {
       round: "[눈썹] 2회차",
       caption: "시술 후 1개월 경과",
-      beforeImageUrl: "./assets/images/main/DUL_2346 copy.png",
+      beforeImageUrl: "./assets/images/main/before-after-sample-1.png",
       beforePosition: "48% 20%",
-      afterImageUrl: "./assets/images/main/sec2-dul-2371.png",
+      afterImageUrl: "./assets/images/main/tattoo-removal-treatment.png",
       afterPosition: "42% 30%"
     },
     {
       round: "[반영구] 4회차",
       caption: "시술 후 3개월 경과",
-      beforeImageUrl: "./assets/images/main/sec2-dul-2371.png",
+      beforeImageUrl: "./assets/images/main/tattoo-removal-treatment.png",
       beforePosition: "74% center",
-      afterImageUrl: "./assets/images/main/DUL_2346 copy.png",
+      afterImageUrl: "./assets/images/main/before-after-sample-1.png",
       afterPosition: "44% center"
     }
   ];
@@ -232,10 +232,10 @@ $(function () {
     var html = "";
 
     $.each(menuItems, function (index, label) {
-      html += '<a href="#" class="rectangle-40">' + label + "</a>";
+      html += '<a href="#" class="quick-menu-link">' + label + "</a>";
     });
 
-    $hoverQuickMenu.html(html);
+    $headerQuickMenu.html(html);
   }
 
   function renderSec2MainTitle(mainTitleParts) {
